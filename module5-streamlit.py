8aa212f8b1ba85b533a4171f30fa7f683999437f
mpearc11 <mpearc11@jhu.edu>
2025-09-25 16:02:11 -0400

    Update module5-streamlit.py

--- module5-streamlit.py
+++ module5-streamlit.py
@@ -93,7 +93,7 @@ if st.button('read in PSA alignment'):
             #df_combined = pd.concat([df_exploded, consurf_df], axis=1)
             #st.write(df_combined)
 
-            r = 0
+            r = 1
             for i in consurf_df['COLOR']:
                 st.write(i)
                 for aa in df_exploded[r,'ps seq']:
