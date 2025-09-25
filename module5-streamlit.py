a08db1b45623ee6341b27632d920a3aebaa15f23
mpearc11 <mpearc11@jhu.edu>
2025-09-25 15:03:24 -0400

    Update module5-streamlit.py

--- module5-streamlit.py
+++ module5-streamlit.py
@@ -92,7 +92,7 @@ if st.button('read in PSA alignment'):
             #st.write(df_combined)
 
             for i in consurf_df['COLOR']:
-                df_exploded['color'] = np.where(df_exploded['ps seq'] != '-' , 'found', 'no match')
+                df_exploded['color'] = np.where(df_exploded['ps seq'] != '-' and != '', 'found', 'no match')
             #for i in consurf_df['COLOR']:
                 #st.write(i)
                 #if row in df_exploded['ps seq'] is not '-' or '': #will need to edit bc i think using 'i' means that things will ony paste in the same row, need to be able to shift rows
