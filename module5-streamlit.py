72e0b094894646f95e7a4b5b38430f1d97aa8346
mpearc11 <mpearc11@jhu.edu>
2025-09-28 17:29:14 -0400

    Update module5-streamlit.py

--- module5-streamlit.py
+++ module5-streamlit.py
@@ -37,9 +37,9 @@ else:
 
 #temp = psa_file.read() ##adds 'b in front of file & other character issues (adds /n etc)
 bytes = psa_file.getvalue() ##adds 'b in front of file & other character issues (adds /n etc)
-st.write(bytes)
+#st.write(bytes)
 temp = psa_file.getvalue().decode("utf-8") ##decodes characters correctly but still has too long file name issue
-st.text(temp)
+#st.text(temp)
 
 #declaring variables outside of button if statement so i can access them after the button step
 df = ''
@@ -51,31 +51,31 @@ if st.button('read in clustal alignment file'):
     #alignment = AlignIO.read(temp, 'clustal')
     alignment = AlignIO.read(StringIO(temp), "clustal")
     #alignment = AlignIO.read('ctei_clustal.aln', 'clustal')
-    st.write(alignment)
+    #st.write(alignment)
     
     #convert clustal alignment to individual sequence strings
     
     seq1 = str(alignment[0].seq)
     seq2 = str(alignment[1].seq)
     
-    st.write(seq1)
-    st.write(seq2)
+    #st.write(seq1)
+    #st.write(seq2)
     
     #convert strings to pandas dataframe
     
     data = {'target seq': [seq1],
                 'ps seq': [seq2]}
     df = pd.DataFrame(data)
-    st.write(df)
+    #st.write(df)
     df1 = df['target seq'].str.split('').explode().reset_index(drop=True)
-    st.write(df1)
+    #st.write(df1)
     df2 = df['ps seq'].str.split('').explode().reset_index(drop=True)
-    st.write(df2)
+    #st.write(df2)
     df_exploded = pd.concat([df1, df2], axis=1)
-    st.write(df_exploded)
+    #st.write(df_exploded)
     #df_exploded['color'] = 0
     #df_exploded = df_exploded.iloc[1:].reset_index(drop=True) #moving this to after the conservation symbols are added
-    st.write(df_exploded)
+    #st.write(df_exploded)
        
     #declaring more variables outside button if statement
     consurf_df = ''
@@ -91,23 +91,23 @@ if st.button('read in clustal alignment file'):
                 # Extract only the symbols, stripping whitespace
                 symbols = line[26:] #.strip() #not using .strip() bc i need spaces; dropping the first 26 bc that seems to be the number of excess at the front in the file (should be consistent btwn alignments)
                 conservation_line += symbols
-    st.write(conservation_line)
+    #st.write(conservation_line)
     co_data = {'conservation': [conservation_line]}
     df_symbols = pd.DataFrame(co_data)
     df_symbols = df_symbols['conservation'].str.split('').explode().reset_index(drop=True)
     df_exploded = pd.concat([df_exploded, df_symbols], axis=1)
     df_exploded = df_exploded.iloc[1:].reset_index(drop=True)
-    st.write(df_exploded)
+    #st.write(df_exploded)
     
 
     @st.fragment()
     def frag():
         if st.button('create consurf dataframe & align with clustal aln dataframe'):
             consurf_df = pd.read_csv(consurf_file)
-            st.write(consurf_df)
+            #st.write(consurf_df)
             consurf_df = consurf_df[['SEQ','COLOR']]
             consurf_df = consurf_df.iloc[1:].reset_index(drop=True)
-            st.write(consurf_df)
+            #st.write(consurf_df)
             
             #combine dataframes; can concat OR just create the new COLOR one based on presence/absence of letter in each row
             
@@ -118,32 +118,32 @@ if st.button('read in clustal alignment file'):
             for idx, aa in enumerate(df_exploded['ps seq']):
                 #st.write(aa)
                 if aa == '-':
-                    st.write(idx)
+                    #st.write(idx)
                     gap = idx
-                    st.write(gap)
-                    st.write(gap - 0.5)
+                    #st.write(gap)
+                    #st.write(gap - 0.5)
                     #consurf_df.loc[gap] = ''
                     line = DataFrame({"SEQ": '', "COLOR": 0}, index=[gap -0.5])
                     consurf_df = pd.concat([consurf_df, line])
                     consurf_df = consurf_df.sort_index().reset_index(drop=True)
-            st.write(consurf_df)
+            #st.write(consurf_df)
             df_combined = pd.concat([df_exploded, consurf_df], axis=1)
             df_combined = df_combined.iloc[:-1]
-            st.write(df_combined)
+            #st.write(df_combined)
 
             #create new column (evoscore) and fill cells
             df_combined['evoscore'] = ''
-            st.write(df_combined)
-            st.write(df_combined.dtypes)
+            #st.write(df_combined)
+            #st.write(df_combined.dtypes)
             for idx,i in enumerate(df_combined['COLOR']):
                 if i < 4:
                     df_combined.iloc[idx,5] = 0
                 if i >= 4:
                     df_combined.iloc[idx,5] = i
-            st.write(df_combined)
-            st.write(df_combined.dtypes)
+            #st.write(df_combined)
+            #st.write(df_combined.dtypes)
             df_combined['evoscore'] = df_combined['evoscore'].astype(float)
-            st.write(df_combined.dtypes)
+            #st.write(df_combined.dtypes)
             evoscore = df_combined['evoscore'].sum()
             st.write('unweighted evoscore = ' + str(evoscore))
             
